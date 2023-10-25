73ce854c267b5537179e0718aa7df5652e41395e
yunjeong.seo <yunjbly2470@naver.com>
2023-10-25 11:16:34 +0900

    :sparkles: Feat: primitive types

---
+++ index.js
@@ -0,0 +1,4 @@
+var name = "dubu";
+var isMarried = false;
+var members = ["kim", "lee"];
+var member = { member1: "kim", member2: "lee" };
---
+++ index.ts
@@ -0,0 +1,4 @@
+const name: string = "dubu";
+const isMarried: boolean = false;
+const members: string[] = ["kim", "lee"];
+const member: { member1: string; member2: string } = { member1: "kim", member2: "lee" };
