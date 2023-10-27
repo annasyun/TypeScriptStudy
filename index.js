b176d0f64e6b595135817102ea3da0a556f89cb3
yunjeong.seo <yunjbly2470@naver.com>
2023-10-27 21:16:19 +0900

    :speech_balloon: Type alias & readonly

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
-//Type Assertion<-버그 잡기 힘들기 때문에 지양
-function 내함수(x) {
-    return x + 1;
-}
-console.log(내함수(123));
+var animal;
+var pet = { name: "dubu", age: 9 };
--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
-//Type Assertion<-버그 잡기 힘들기 때문에 지양
-function 내함수(x: number | string) {
-  return (x as number) + 1;
-}
-console.log(내함수(123));
+// Type Aliases (별칭)
+type AniamlType = string | number | boolean;
+let animal: aniamlType;
+
+//readonly
+type PetType = { readonly name: string; readonly age: number };
+const pet: PetType = { name: "dubu", age: 9 };
