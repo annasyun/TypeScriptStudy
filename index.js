31c74653df88714f38dfd93e2110882178c7822b
yunjeong.seo <yunjbly2470@naver.com>
2023-10-31 23:28:57 +0900

    :speech_balloon: Class 타입 지정 하는 방법

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-// a타입 선택자 HTML 변경하는 방법
-var 링크 = document.querySelector('#link');
-if (링크 instanceof HTMLAnchorElement) {
-    링크.href = 'https://kakao.com'; //잘됨
-}
+var Person = /** @class */ (function () {
+    function Person(a) {
+        this.name = a;
+        this.age = 20;
+    }
+    return Person;
+}());
--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
-// a타입 선택자 HTML 변경하는 방법
-let 링크 = document.querySelector('#link');
-if (링크 instanceof HTMLAnchorElement) {
-  링크.href = 'https://kakao.com'  //잘됨
+class Person {
+  name;
+  age;
+  constructor(a: string) {
+    this.name = a;
+    this.age = 20;
+  }
 }
