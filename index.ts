7ef92ac99c5612f48205e34555b6ddc735453666
yunjeong.seo <yunjbly2470@naver.com>
2023-11-08 22:38:01 +0900

    :speech_balloon: In 연산자로 object 자료 narrowing 하는 방법

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
-function printAll(strs: string | undefined) {
-  if (strs && typeof strs === "string") {  
-    console.log(s);
-  } 
-}
+type Fish = { swim: string };
+type Bird = { fly: string };
+function 함수(animal: Fish | Bird) {
+  if ("swim" in animal) {
+    return animal.swim
+  }
+  return animal.fly
+} 
