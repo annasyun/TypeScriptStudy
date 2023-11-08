7f04617d3cb3d0bce4855593e1b096eba780e728
yunjeong.seo <yunjbly2470@naver.com>
2023-11-08 22:40:34 +0900

    :speech_balloon: Instanceof와literal type활용한 narrowing

--- index.ts
+++ index.ts
@@ -1,8 +1,24 @@
-type Fish = { swim: string };
-type Bird = { fly: string };
-function 함수(animal: Fish | Bird) {
-  if ("swim" in animal) {
-    return animal.swim
+//class로부터 생산된 object라면 instanceof로 narrowing
+
+let 날짜 = new Date();
+if (날짜 instanceof Date){
+  console.log('참이에요')
+}
+
+//literal type활용한 narrowing
+type Car = {
+  wheel : '4개',
+  color : string
+}
+type Bike = {
+  wheel : '2개',
+  color : string
+}
+
+function 함수(x : Car | Bike){
+  if (x.wheel === '4개'){
+    console.log('the car is ' + x.color)
+  } else {
+    console.log('the bike is ' + x.color)
   }
-  return animal.fly
-} 
+}
